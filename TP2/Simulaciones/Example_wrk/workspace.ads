ad58d8609a097ebbd188c96f0622e44a34694a53
AbelCorvalan0 <abel.corvalan@mi.unc.edu.ar>
2024-04-30 14:11:53 -0300

    Add simulations (Our case and example)

---
+++ TP2/Simulaciones/Example_wrk/workspace.ads
@@ -0,0 +1,36 @@
+<ADSWorkspace Revision="1" Version="100">
+    <Workspace Name="">
+        <Library Name="ads_standard_layers" />
+        <Library Name="adstechlib" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_verification_test_bench" />
+        <Library Name="Example_lib" />
+        <Library Name="Infineon_RF_tech" />
+        <Library Name="Infineon_RF" />
+        <Cell Name="Example_lib:s_p" />
+        <Dataset Name="s_p.ds" />
+        <Log Name="netlist.log" />
+        <Log Name="search_history.log" />
+        <Preferences Name="Example_lib_lay.prf" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="schematic.prf" />
+        <LibraryDefs Name="lib.defs" />
+        <ConfigFile Name="dds.cfg" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+    </Workspace>
+</ADSWorkspace>
---
+++ TP2/Simulaciones/MyWorkspace_wrk/workspace.ads
@@ -0,0 +1,40 @@
+<ADSWorkspace Revision="5" Version="100">
+    <Workspace Name="">
+        <Library Name="ads_standard_layers" />
+        <Library Name="adstechlib" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_verification_test_bench" />
+        <Library Name="MyLibrary_lib" />
+        <Library Name="Infineon_RF_tech" />
+        <Library Name="Infineon_RF" />
+        <Cell Name="MyLibrary_lib:Transistor_Test" />
+        <Dataset Name="cell_1.ds" />
+        <Dataset Name="Transistor_Components.ds" />
+        <Log Name="netlist.log" />
+        <Log Name="search_history.log" />
+        <Preferences Name="ads_datacmps_lay.prf" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="MyLibrary_lib_lay.prf" />
+        <Preferences Name="schematic.prf" />
+        <LibraryDefs Name="lib.defs" />
+        <ConfigFile Name="dds.cfg" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+        <Dataset Name="Transistor_Test.ds" />
+        <Data_Display Name="Transistor_Test.dds" />
+    </Workspace>
+</ADSWorkspace>
